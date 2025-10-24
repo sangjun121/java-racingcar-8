c2d103cbf42e46a8bfe7975b25234ef2fd304c76
조상준 <juncho12011201@gmail.com>
2025-10-24 20:27:15 +0200

    refactor: 도메인 Car 구현을 vo에서 상태 변경가능한 엔티티 형태로 변경
    - Car 객체는 상태 변화가 가능한 객체라고 판단.
    - 불변 객체를 설계하기 보다는, 상태 변경이 안전하게 관리되도록 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,28 +4,19 @@ import racingcar.exception.InvalidCarException;
 import racingcar.exception.Message;
 import racingcar.util.Validator;
 
-import java.util.Objects;
-
-public final class Car {
+public class Car {
     private static final int MIN_CAR_NAME_LENGTH = 1;
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static final int INITIAL_DISTANCE = 0;
     private static final int MOVE_UNIT = 1;
 
     private final String name;
-    private final int distance;
+    private int distance;
 
     public Car(String name) {
         validateName(name);
         this.name = name;
         this.distance = INITIAL_DISTANCE;
-    }
-
-    private Car(String name, int distance) {
-        validateName(name);
-        validateDistance(distance);
-        this.name = name;
-        this.distance = distance;
     }
 
     public String getName() {
@@ -36,17 +27,13 @@ public final class Car {
         return distance;
     }
 
-    public Car move() {
-        return new Car(this.name, this.distance + MOVE_UNIT);
+    public void move() {
+        this.distance += MOVE_UNIT;
     }
 
     private void validateName(String name) {
         validateNameNotNullOrBlank(name);
         validateNameLength(name);
-    }
-
-    private void validateDistance(int distance) {
-        validateDistanceIsNonNegative(distance);
     }
 
     private void validateNameNotNullOrBlank(String name) {
@@ -57,23 +44,5 @@ public final class Car {
     private void validateNameLength(String name) {
         if (Validator.isWithinLengthRange(name, MIN_CAR_NAME_LENGTH, MAX_CAR_NAME_LENGTH)) return;
         throw new InvalidCarException(Message.CAR_NAME_LENGTH_OUT_OF_RANGE.getMessage());
-    }
-
-    private void validateDistanceIsNonNegative(int distance) {
-        if (!Validator.isLessThan(distance, INITIAL_DISTANCE)) return;
-        throw new InvalidCarException(Message.CAR_DISTANCE_NEGATIVE.getMessage());
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (!(object instanceof Car other)) return false;
-        return distance == other.distance
-                && Objects.equals(name, other.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, distance);
     }
 }
