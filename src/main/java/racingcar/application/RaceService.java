fa2933ba775a4317b1a2c5005f135747f826270c
조상준 <juncho12011201@gmail.com>
2025-10-24 23:50:14 +0200

    feat(racingcar): 전체 라운드 실행 및 각 라운드 실행 결과 값 반환 로직 작성

--- src/main/java/racingcar/application/RaceService.java
+++ src/main/java/racingcar/application/RaceService.java
@@ -1,13 +1,46 @@
 package racingcar.application;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+import racingcar.domain.Result;
 import racingcar.exception.InvalidInputException;
 import racingcar.exception.Message;
 import racingcar.util.Validator;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class RaceService {
     // TODO: 반환 값 Result로 수정
     public void run(String carNamesInput, String tryCountInput) {
+        Cars cars = new Cars(carNamesInput);
         int tryCount = parseTryCount(tryCountInput);
+
+        List<Result.RoundResult> roundResults = startRace(cars, tryCount);
+    }
+
+    private List<Result.RoundResult> startRace(Cars cars, int tryCount) {
+        List<Result.RoundResult> roundResults = new ArrayList<>();
+
+        for (int roundNumber = 1; roundNumber <= tryCount; roundNumber++) {
+            Result.RoundResult roundResult = startRound(roundNumber, cars);
+            roundResults.add(roundResult);
+        }
+
+        return roundResults;
+    }
+
+    private Result.RoundResult startRound(int roundNumber, Cars cars) {
+        Map<String, Integer> roundResult = new HashMap<>();
+        cars.move();
+
+        for (Car car : cars.getCars()) {
+            roundResult.put(car.getName(), car.getDistance());
+        }
+
+        return new Result.RoundResult(roundNumber, roundResult);
     }
 
     private int parseTryCount(String input) {
