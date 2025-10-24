f765705a6a8619db1600dba43e7bc73c1b2bcaeb
조상준 <juncho12011201@gmail.com>
2025-10-25 00:13:17 +0200

    feat(view): 실행 결과 및 우승자 출력 로직 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,15 +1,33 @@
 package racingcar.view;
 
+import racingcar.domain.Result;
+
+import java.util.List;
+import java.util.Map;
+
 public class OutputView {
-    private static final String RACE_RESULT_GUIDE_MESSAGE = "실행 결과";
+    private static final String RACE_RESULT_GUIDE_MESSAGE = "\n실행 결과";
+    private static final String RACE_WINNER_GUIDE_MESSAGE = "최종 우승자 : ";
     private static final String NAME_DISTANCE_SEPARATOR = " : ";
     private static final String DISTANCE_SYMBOL = "-";
+    private static final String WINNER_SEPARATOR = ", ";
 
-    public void printRaceResultGuideMessage() {
+    public void printRaceResult(List<Result.RoundResult> roundResults) {
         System.out.println(RACE_RESULT_GUIDE_MESSAGE);
+        for (Result.RoundResult roundResult : roundResults) {
+            printRoundResult(roundResult.carPositions());
+        }
     }
 
-    public void printRoundResult(String carName, int distance) {
-        System.out.println(carName + NAME_DISTANCE_SEPARATOR + DISTANCE_SYMBOL.repeat(distance));
+    public void printWinners(List<String> winners) {
+        String winnersString = String.join(WINNER_SEPARATOR, winners);
+        System.out.println(RACE_WINNER_GUIDE_MESSAGE + winnersString);
+    }
+
+    private void printRoundResult(Map<String, Integer> roundResult) {
+        for (Map.Entry<String, Integer> entry : roundResult.entrySet()) {
+            System.out.println(entry.getKey() + NAME_DISTANCE_SEPARATOR + DISTANCE_SYMBOL.repeat(entry.getValue()));
+        }
+        System.out.println();
     }
 }
