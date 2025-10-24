e6833193d5e5621d9c1a2b7eccb9032f26402d1a
조상준 <juncho12011201@gmail.com>
2025-10-24 23:06:45 +0200

    feat(Result): 결과 값 반환용 DTO 구현

---
+++ src/main/java/racingcar/domain/Result.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import java.util.List;
+import java.util.Map;
+
+public record Result(
+        List<RoundResult> roundResults,
+        List<String> winners
+) {
+
+    public Result {
+        roundResults = List.copyOf(roundResults);
+        winners = List.copyOf(winners);
+    }
+
+    public record RoundResult(int roundNumber,
+                              Map<String, Integer> carPositions) {
+
+        public RoundResult {
+            carPositions = Map.copyOf(carPositions);
+        }
+    }
+}
